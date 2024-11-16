5099cfe263ca3ddafaa48051d6b99d887d38e0c1
heasheartfire <gagi1995@gmail.com>
2024-11-16 23:16:53 +0100

    feat: Add generic Result and Error objects to be used for better query and command handling

---
+++ Shared/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace BookHeaven.Domain.Shared;
+
+public sealed record Error(string Code, string? Description = null)
+{
+	public static readonly Error None = new Error(string.Empty);
+
+	public static implicit operator Result(Error error) => Result.Failure(error);
+}
