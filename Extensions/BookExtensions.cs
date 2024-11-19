1966a120e1281a73f960d2d2577bc29108a36458
heasheartfire <gagi1995@gmail.com>
2024-11-19 23:27:44 +0100

    refactor

--- Extensions/BookExtensions.cs
+++ Extensions/BookExtensions.cs
@@ -4,23 +4,9 @@ namespace BookHeaven.Domain.Extensions;
 
 public static class BookExtensions
 {
-    public static bool IsStartedReading(this Book book)
-    {
-        return book.Progresses.Any(x => x.ElapsedTime != TimeSpan.Zero);
-    }
-    
-    public static bool IsFinishedReading(this Book book)
-    {
-        return book.Progresses.Any(x => x.EndDate != null);
-    }
-    
-    public static List<Book> GetReadingBooks(this IEnumerable<Book> books)
-    {
-        return books.Where(b => b.IsStartedReading() && !b.IsFinishedReading()).ToList();
-    }
-    
-    public static bool AnyReading(this IEnumerable<Book> books)
-    {
-        return books.Any(b => b.IsStartedReading() && !b.IsFinishedReading());
-    }
+    public static BookProgress? Progress(this Book book) => book.Progresses.FirstOrDefault();
+    public static bool IsStartedReading(this Book book) => book.Progress()?.ElapsedTime != TimeSpan.Zero;
+    public static bool IsFinishedReading(this Book book) => book.Progress()?.EndDate != null;
+    public static List<Book> GetReadingBooks(this IEnumerable<Book> books) => books.Where(b => b.IsStartedReading() && !b.IsFinishedReading()).ToList();
+    public static bool AnyReading(this IEnumerable<Book> books) => books.Any(b => b.IsStartedReading() && !b.IsFinishedReading());
 }
