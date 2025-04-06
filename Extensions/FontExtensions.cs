b3d9cd59f40ea9e0e7eeaa928d4584887942e1a1
heasheartfire <gagi1995@gmail.com>
2025-04-06 17:54:40 +0200

    refactor(Font): Add extension method to get a css font-face

--- Extensions/FontExtensions.cs
+++ Extensions/FontExtensions.cs
@@ -7,4 +7,14 @@ public static class FontExtensions
     public static string Folder(this Font font, string basePath) => Path.Combine(basePath, font.Family);
     public static string File(this Font font, string basePath) => Path.Combine(basePath, font.Family, font.FileName);
     public static string Url(this Font font) => $"/fonts/{font.Family}/{font.FileName}";
+
+    public static string GetFontFace(this Font font)
+    {
+        return $@"@font-face {{
+            font-family: '{font.Family}';
+            src: url('{font.Url()}');
+            font-weight: {font.Weight};
+            font-style: {font.Style};
+        }}";
+    }
 }
