093da477633742affdd99db00d5ed6c38a595078
heasheartfire <gagi1995@gmail.com>
2025-04-08 10:08:29 +0200

    refactor(Broadcast): Change logic

--- Constants/Broadcast.cs
+++ Constants/Broadcast.cs
@@ -3,7 +3,7 @@
 public static class Broadcast
 {
     public const int BROADCAST_PORT = 27007;
-    public const string DISCOVER_MESSAGE = "BOOKHEAVEN-DISCOVER";
+    public const string DISCOVER_MESSAGE_PREFIX = "BOOKHEAVEN-DISCOVER:";
     public const string SERVER_URL_MESSAGE_PREFIX = "BOOKHEAVEN-SERVER:";
     public const string ACK_MESSAGE = "ACK-BOOKHEAVEN";
 }
