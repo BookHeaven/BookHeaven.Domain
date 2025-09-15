d2a6d9400718072b56a078d84bcfb89075cb4de7
heasheartfire <gagi1995@gmail.com>
2025-09-15 23:21:10 +0200

    feat(Utilities): add StoreFile method for file storage from source

---
+++ Shared/Utilities.cs
@@ -0,0 +1,34 @@
+﻿namespace BookHeaven.Domain.Shared;
+
+public static class Utilities
+{
+    public static async Task StoreFile(string? source, string destination, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrWhiteSpace(source))
+        {
+            return;
+        }
+            
+        byte[] fileBytes;
+        if (source.StartsWith("http"))
+        {
+            using var httpClient = new HttpClient();
+            fileBytes = await httpClient.GetByteArrayAsync(source, cancellationToken);
+        }
+        else
+        {
+            fileBytes = await File.ReadAllBytesAsync(source, cancellationToken);
+        }
+        
+        var dir = Path.GetDirectoryName(destination);
+        if (!string.IsNullOrEmpty(dir) && !Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+
+        if (fileBytes.Length > 0)
+        {
+            await File.WriteAllBytesAsync(destination, fileBytes, cancellationToken);
+        }
+    }
+}
