ab00296beea86a72546d05d306f992a6811e7acc
heasheartfire <gagi1995@gmail.com>
2025-09-15 10:07:34 +0200

    feat(Tags): add GetAllTags query to retrieve all tags from the database

---
+++ Features/Tags/GetAllTags.cs
@@ -0,0 +1,18 @@
+﻿using BookHeaven.Domain.Entities;
+
+namespace BookHeaven.Domain.Features.Tags;
+
+public static class GetAllTags
+{
+    public sealed record Query : IQuery<List<Tag>>;
+
+    internal class Handler(IDbContextFactory<DatabaseContext> dbContextFactory) : IQueryHandler<Query, List<Tag>>
+    {
+        public async Task<Result<List<Tag>>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            await using var dbContext = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+
+            return await dbContext.Tags.ToListAsync(cancellationToken);
+        }
+    }
+}
