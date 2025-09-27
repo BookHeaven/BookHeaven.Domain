da8a475de1a880e65f18f31c1b8877d4940396aa
heasheartfire <gagi1995@gmail.com>
2025-09-27 13:19:21 +0200

    feat(DeleteSeries): implement command to delete a book series and handle associated books

---
+++ Features/BookSeries/DeleteSeries.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace BookHeaven.Domain.Features.BookSeries;
+
+public static class DeleteSeries
+{
+    public sealed record Command(Guid SeriesId) : ICommand;
+    
+    internal class Handler(
+        ILogger<Handler> logger,
+        IDbContextFactory<DatabaseContext> dbContextFactory) : ICommandHandler<Command>
+    {
+        public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
+        {
+            await using var dbContext = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+            var series = await dbContext.Series
+                .Include(bs => bs.Books)
+                .FirstOrDefaultAsync(bs => bs.SeriesId == request.SeriesId, cancellationToken);
+            if (series is null)
+            {
+                return new Error("Series not found.");
+            }
+            
+            if (series.Books.Count != 0)
+            {
+                foreach (var book in series.Books)
+                {
+                    book.SeriesIndex = null;
+                }
+            }
+
+            try
+            {
+                dbContext.Remove(series);
+                await dbContext.SaveChangesAsync(cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error deleting series with ID {SeriesId}", request.SeriesId);
+                return new Error("Failed to delete series.");
+            }
+            
+            return Result.Success();
+        }
+    }
+}
