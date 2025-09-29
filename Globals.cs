aad6632306a90bb52fa01356992815f0fd65e3e2
heasheartfire <gagi1995@gmail.com>
2025-09-29 21:00:23 +0200

    feat(Globals): add SupportedFormats property

--- Globals.cs
+++ Globals.cs
@@ -5,4 +5,10 @@ internal static class Globals
     public static string BooksPath { get; set; } = string.Empty;
     public static string CoversPath { get; set; } = string.Empty;
     public static string FontsPath { get; set; } = string.Empty;
+
+    public static IReadOnlyList<string> SupportedFormats { get; } =
+    [
+        ".epub",
+        ".pdf"
+    ];
 }
