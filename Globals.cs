f8bf874a7d7039cce74f41d05c0473901c589869
heasheartfire <gagi1995@gmail.com>
2025-09-29 21:01:48 +0200

    refactor(Globals): change class visibility from internal to public

--- Globals.cs
+++ Globals.cs
@@ -1,10 +1,10 @@
 ﻿namespace BookHeaven.Domain;
 
-internal static class Globals
+public static class Globals
 {
-    public static string BooksPath { get; set; } = string.Empty;
-    public static string CoversPath { get; set; } = string.Empty;
-    public static string FontsPath { get; set; } = string.Empty;
+    internal static string BooksPath { get; set; } = string.Empty;
+    internal static string CoversPath { get; set; } = string.Empty;
+    internal static string FontsPath { get; set; } = string.Empty;
 
     public static IReadOnlyList<string> SupportedFormats { get; } =
     [
