9c5dfe29d48b6be18daeda43cede6a6cd6e04e5b
heasheartfire <gagi1995@gmail.com>
2026-01-10 00:56:08 +0100

    feat(authors): add delete author functionality

---
+++ Features/Authors/DeleteAuthor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace BookHeaven.Domain.Features.Authors;
+
+public static class DeleteAuthor
+{
+    public sealed record Command(Guid AuthorId) : ICommand;
+    
+    internal class Handler(
+        IDbContextFactory<DatabaseContext> dbContextFactory,
+        ILogger<Handler> logger) : ICommandHandler<Command>
+    {
+        public async Task<Result> Handle(Command command, CancellationToken cancellationToken = default)
+        {
+            await using var dbContext = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+            var author = await dbContext.Authors
+                .FirstOrDefaultAsync(a => a.AuthorId == command.AuthorId, cancellationToken);
+
+
+            if (author is null)
+            {
+                return new Error("Author not found");
+            }
+
+            try
+            {
+                
+                dbContext.Authors.Remove(author);
+                await dbContext.SaveChangesAsync(cancellationToken);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "Failed to delete author");
+                return new Error("An error occurred while deleting the author.");
+            }
+            return Result.Success();
+        }
+    }
+}
