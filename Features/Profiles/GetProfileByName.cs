7e7e692043ee78605ca45bf979ff71eeb1faa057
heasheartfire <gagi1995@gmail.com>
2026-03-18 16:52:46 +0100

    feat(GetProfileByName): add query handler to retrieve profile by name

---
+++ Features/Profiles/GetProfileByName.cs
@@ -0,0 +1,19 @@
+﻿using BookHeaven.Domain.Entities;
+
+namespace BookHeaven.Domain.Features.Profiles;
+
+public static class GetProfileByName
+{
+    public sealed record Query(string Name) : IQuery<Profile>;
+    
+    internal class Handler(IDbContextFactory<DatabaseContext> dbContextFactory) : IQueryHandler<Query, Profile>
+    {
+        public async Task<Result<Profile>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            await using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+            var profile = await context.Profiles.FirstOrDefaultAsync(p => p.Name == request.Name, cancellationToken: cancellationToken);
+            
+            return profile != null ? profile : new Error("Profile not found");
+        }
+    }
+}
