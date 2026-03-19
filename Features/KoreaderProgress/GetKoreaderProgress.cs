12f6eddd9f7211cc266e22bd94a84c011d92ba81
heasheartfire <gagi1995@gmail.com>
2026-03-19 18:23:18 +0100

    feat(KoreaderProgress): implement Get and Save use cases for KoreaderProgress

---
+++ Features/KoreaderProgress/GetKoreaderProgress.cs
@@ -0,0 +1,22 @@
+﻿namespace BookHeaven.Domain.Features.KoreaderProgress;
+
+public static class GetKoreaderProgress
+{
+    public sealed record Query(Guid ProfileId, string DocumentHash) : IQuery<Entities.KoreaderProgress>;
+    
+    internal class Handler(IDbContextFactory<DatabaseContext> dbContextFactory) : IQueryHandler<Query, Entities.KoreaderProgress>
+    {
+        public async Task<Result<Entities.KoreaderProgress>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            await using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+            
+            var progress = await context.KoreaderProgress
+                .FirstOrDefaultAsync(kp => 
+                    kp.ProfileId == request.ProfileId && 
+                    kp.DocumentHash == request.DocumentHash, 
+                    cancellationToken);
+            
+            return progress != null ? progress : new Error("Progress not found");
+        }
+    }
+}
---
+++ Features/KoreaderProgress/SaveKoreaderProgress.cs
@@ -0,0 +1,43 @@
+﻿namespace BookHeaven.Domain.Features.KoreaderProgress;
+
+public static class SaveKoreaderProgress
+{
+    public sealed record Command(Entities.KoreaderProgress Progress) : ICommand;
+    
+    internal class Handler(IDbContextFactory<DatabaseContext> dbContextFactory) : ICommandHandler<Command>
+    {
+        public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
+        {
+            await using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+            
+            var koProgress = await context.KoreaderProgress
+                .FirstOrDefaultAsync(kp => 
+                    kp.ProfileId == request.Progress.ProfileId && 
+                    kp.DocumentHash == request.Progress.DocumentHash, 
+                    cancellationToken);
+
+            if (koProgress is null)
+            {
+                await context.AddAsync(request.Progress, cancellationToken);
+            }
+            else
+            {
+                koProgress.DeviceId = request.Progress.DeviceId;
+                koProgress.DeviceName = request.Progress.DeviceName;
+                koProgress.Percentage = request.Progress.Percentage;
+                koProgress.Progress = request.Progress.Progress;
+                koProgress.Timestamp = request.Progress.Timestamp;
+            }
+
+            try
+            {
+                await context.SaveChangesAsync(cancellationToken);
+            }
+            catch (Exception e)
+            {
+                return new Error(e.Message);
+            }
+            return Result.Success();
+        }
+    }
+}
